0bbc9fb533847ae337656a2aa0f8c75119c94205
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-04-16 20:05:20 +0200

    Restored events

---
+++ src/Event/ConstructedEvent.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of CakeDI.
+ *
+ ** (c) 2016 Cees-Jan Kiewiet
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace WyriHaximus\Cake\DI\Event;
+
+use Cake\Event\Event;
+use Interop\Container\ContainerInterface;
+
+final class ConstructedEvent extends Event
+{
+    const EVENT = 'WyriHaximus.DI.container.constructed';
+
+    /**
+     * @param ContainerInterface $container
+     * @return static
+     */
+    public static function create(ContainerInterface $container)
+    {
+        return new static(static::EVENT, $container, [
+            'container' => $container,
+        ]);
+    }
+
+    /**
+     * @return ContainerInterface
+     */
+    public function getContainer(): ContainerInterface
+    {
+        return $this->data()['container'];
+    }
+}
